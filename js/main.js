7b5f1664ad4ed4fae2b94ee421ddd42e2e8475ce
adam wood <a_wood60810@fanshaweonline.ca>
2019-03-23 04:41:47 -0400

    added sigil animations

--- js/main.js
+++ js/main.js
@@ -6,12 +6,13 @@
 			video = document.querySelector('video'),
 			closeLB = document.querySelector('.lightbox-close'),
 			banners = document.querySelector('#houseImages');
+    let     icon = document.querySelector('.sigil-box');
 
 	function showLightbox() {
 		//grab the right video source
 		//debugger;
 		//get the lowercase house name from the class list
-		let targetHouse = this.className.split(" ")[1];
+		let targetHouse = this;
 		// make sure the names match - needs to be uppercase
 		// stark becomes Stark -> first make a capital S then add tark
 		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
@@ -26,11 +27,21 @@
 
 	function hideLightbox() {
 		lightbox.classList.remove('show-lightbox');
+        icon.classList.remove('show-lightbox');
 		// rewind the video to the beginning
 		// and also pause it
 		video.currentTime = 0;
 		video.pause();
 	}
+    
+    function animateSigil() {
+        
+        icon.classList.add('show-lightbox');
+        
+        icon.innerHTML = `<img src="images/house-${this}.svg" alt="${this}">`;
+        let sigil = document.querySelector('.sigil-box img');
+        TweenMax.fromTo(sigil, 2, { scale: 1, opacity:0 }, { scale: 1.2, repeat:1, yoyo:true, opacity:1 }).eventCallback("onComplete", showLightbox, null, this);
+    }
 
 	function animateBanner() {
 		const offSet = 600;
@@ -43,7 +54,9 @@
 		//set the style css will animate
 		//banners.style.right = totalOffset;
 
-		TweenMax.to(banners, 0.8, { right: totalOffset });
+		TweenMax.to(banners, 0.8, { right: totalOffset }).eventCallback("onComplete", animateSigil, null, nameSrc);
+        
+        
 	}
 
 	//shields.forEach(shield => shield.addEventListener('click', showLightbox));
